78448673ecb2272db49cf8ca7294bf39db57b41a
yoonware <47264613+yoonware@users.noreply.github.com>
2019-11-24 21:48:29 +0900

    2019.11.24 / Quick Sort / modify convention

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -15,40 +15,40 @@ void quick_sort(int* p, int left, int right) {
 	int pivot = p[(l + r) / 2];
 
 	while(1) {
-		while (p[++l] < pivot);  // pivot º¸´Ù Å« °ª Ã£±â
-		while (p[--r] > pivot);  // pivot º¸´Ù ÀÛÀº °ª Ã£±â
-		if (l >= r) break;       // pivot ±âÁØ Á¤·ÄÀÌ ³¡³µÀ» ¶§
+		while (p[++l] < pivot);  // pivot ë³´ë‹¤ í° ê°’ ì°¾ê¸°
+		while (p[--r] > pivot);  // pivot ë³´ë‹¤ ì‘ì€ ê°’ ì°¾ê¸°
+		if (l >= r) break;       // pivot ê¸°ì¤€ ì •ë ¬ì´ ëë‚¬ì„ ë•Œ
 		int temp = p[l];         // swap(p[l], p[r])
 		p[l] = p[r];
 		p[r] = temp;
 	}
 
-	quick_sort(p, left, l - 1);  // pivot ¿ŞÂÊ ¹è¿­
-	quick_sort(p, r + 1, right); // pivot ¿À¸¥ÂÊ ¹è¿­
+	quick_sort(p, left, l - 1);      // pivot ì™¼ìª½ ë°°ì—´
+	quick_sort(p, r + 1, right);     // pivot ì˜¤ë¥¸ìª½ ë°°ì—´
 }
 
 int main()
 {
-	// ÄÉÀÌ½º »ı¼º
+	// ì¼€ì´ìŠ¤ ìƒì„±
 	for (int i = 0; i < ARR_SIZE; ++i)
 		arr_quick[i] = arr_stl[i] = rand();
 
-	// quick Á¤·Ä
+	// quick ì •ë ¬
 	int quick_begin = GetTickCount64();
 	quick_sort(arr_quick, 0, ARR_SIZE - 1);
 	int quick_end = GetTickCount64();
 
-	// stl Á¤·Ä
+	// stl ì •ë ¬
 	int stl_begin = GetTickCount64();
 	sort(arr_stl, arr_stl + ARR_SIZE);
 	int stl_end = GetTickCount64();
 
-	// Á¤È®¼º Ã¼Å©
+	// ì •í™•ì„± ì²´í¬
 	for (int i = 0; i < ARR_SIZE; ++i)
 		if (arr_quick[i] != arr_stl[i])
 			cout << "Error" << endl;
 
-	// ¼Ò¿ä½Ã°£ Ãâ·Â
+	// ì†Œìš”ì‹œê°„ ì¶œë ¥
 	cout << "quick : " << (quick_end - quick_begin) << endl;
 	cout << "stl   : " << (stl_end - stl_begin) << endl;
 
